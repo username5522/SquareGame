a46eab199be8bbbfa68f670e63fbea1b280ee49d
Username * <oueslatim@pm.me>
2024-08-14 11:39:31 +0100

    explosion particles + slow down time when picking up a key

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -1,17 +1,41 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour
 {
     public GameObject door;
+    public float slowdownFactor = 0.5f;
+    public float slowdownDuration = 1.25f;
+    public float particleDuration = 1.75f;
+    public ParticleSystem particles;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-            Destroy(gameObject);
-            door.SetActive(false);
+            StartCoroutine(CollectKey());
         }
+    }
+
+    private IEnumerator CollectKey()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<Renderer>().enabled = false;
+
+        Time.timeScale = slowdownFactor;
+
+        door.SetActive(false);
+
+        particles.Play();
+
+        yield return new WaitForSecondsRealtime(slowdownDuration);
+
+        Time.timeScale = 1f;
+
+        yield return new WaitForSecondsRealtime(particleDuration - slowdownDuration);
+
+        particles.Stop();
+
+        Destroy(gameObject);
     }
 }
