f73dcdd26226eca884c9a50464a8651fffe4f2a7
Username * <oueslatim@pm.me>
2024-08-14 11:37:24 +0100

    only shake the camera when colliding with gameobjects with specific tags

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -9,15 +9,16 @@ public class KillPlayer : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        cameraShake.SmoothShake(intensity, duration);
-        
+
         if (other.gameObject.CompareTag("Checkpoint"))
         {
+            cameraShake.SmoothShake(intensity, duration);
             respawnPoint = other.transform;
         }
 
         if (other.gameObject.CompareTag("DeathTrigger"))
         {
+            cameraShake.SmoothShake(intensity, duration);
             transform.position = respawnPoint.position;
         }
     }
