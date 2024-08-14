173e691b13e7284ab71e11eabb49bbfbe6ffc6e6
Username * <oueslatim@pm.me>
2024-08-14 11:34:20 +0100

    FUCK the pause menu animations

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -6,41 +6,23 @@ public class PauseMenu : MonoBehaviour
 {
     private bool isPaused = false;
     public GameObject pauseMenu;
-    public Animator pauseIconAnimator;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (isPaused == false)
+            if (!isPaused)
             {
                 Time.timeScale = 0;
                 isPaused = true;
-                pauseMenu.SetActive(isPaused);
-                pauseIconAnimator.SetTrigger("ScaleDown");
+                pauseMenu.SetActive(true);
+                return;
             }
-            else if (isPaused == true)
-            {
-                pauseIconAnimator.SetTrigger("ScaleUp");
-                StartCoroutine(WaitForAnimationToEnd("ScaleUp"));
-                isPaused = false;
-                Time.timeScale = 1;
-            }
+
+            isPaused = false;
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+
         }
-    }
-
-    IEnumerator WaitForAnimationToEnd(string animationName)
-    {
-        yield return new WaitForSeconds(0.2f); // Wait for 20ms
-
-        while (pauseIconAnimator.GetCurrentAnimatorStateInfo(0).IsName(animationName))
-        {
-            yield return null;
-        }
-
-        pauseMenu.SetActive(isPaused);
     }
 }
-
-
-// to do: make the scaledown animation work lmfao
